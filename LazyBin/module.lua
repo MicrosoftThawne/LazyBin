d6b9db1f31b643b2faf7e405223ae60f4431a08d
Thawne <50044561+MicrosoftThawne@users.noreply.github.com>
2020-01-22 20:00:30 +0100

    Update module.lua

--- LazyBin/module.lua
+++ LazyBin/module.lua
@@ -1,9 +1,17 @@
--->>   LazyBin | 1.0.1   <<--
+-->>   LazyBin | 1.0.3   <<--
 -->> Written by JaheedFX <<--
+-->> HAS NOT BEEN TESTED YET! <<--
 
 local lazyBin = {}
-
 local LUA_TTABLE = "table";local LUA_TSTRING = "string";local LUA_TNUMBER = "number";local LUA_TBOOLEAN = "boolean";local LUA_TNIL = nil;local LUA_TUSERDATA = "userdata";local LUA_TFUNCTION = "function"
+local RegisteredKeys      = {}
+local RegisteredFunctions = {}
+local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+
+function lazyBin.RegisterKey(key, event)
+  table.insert(RegisteredKeys,  key)
+  table.insert(RegisteredFunctions,  event)
+end
 
 function lazyBin.match(_1, _2)
   if _1 == _2 then
@@ -13,6 +21,15 @@ function lazyBin.match(_1, _2)
   end
 end
 
+-->> TODO: Add arguments to registered functions! <<--
+Mouse.KeyDown(function(key)
+    for index,value in pairs(RegisteredKeys) do
+      if string.match(tolower(key), tolower(value)) then
+        RegisteredFunctions[index]()
+      end
+    end
+end)
+  
 function lazyBin.ChangeText(object, text)
   object.Text = text
 end
